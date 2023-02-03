0ce7f09224486bc504b0a6649e1b415d151336df
kozerka <kozerka@gmail.com>
2023-02-03 09:53:49 +0100

    Add email validation

--- script.js
+++ script.js
@@ -17,28 +17,47 @@ function showSuccess(input) {
 	formField.className = 'form__field success';
 }
 
+function checkEmail(input) {
+	const re =
+		/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+	if (re.test(input.value.trim())) {
+		showSuccess(input);
+	} else {
+		showError(input, 'Email is not valid');
+	}
+}
+function getInputName(input) {
+	return input.id.charAt(0).toUpperCase() + input.id.slice(1);
+}
+function checkRequire(inputArr) {
+	inputArr.forEach((input) => {
+		if (input.value.trim() === '') {
+			showError(input, `${getInputName(input)} is required`);
+		} else {
+			showSuccess(input);
+		}
+	});
+}
+function checkLength(input, min, max) {
+	if (input.value.length < min) {
+		showError(
+			input,
+			`${getInputName(input)} must be at least ${min} characters long`
+		);
+	} else if (input.value.length > max) {
+		showError(
+			input,
+			`${getInputName(input)} must be less than ${max} characters long`
+		);
+	} else {
+		showSuccess(input);
+	}
+}
+
 form.addEventListener('submit', function (e) {
 	e.preventDefault();
-
-	if (username.value === '') {
-		showError(username, 'Username is required');
-	} else {
-		showSuccess(username);
-	}
-	if (email.value === '') {
-		showError(email, 'Username email is required');
-	} else {
-		showSuccess(email);
-	}
-
-	if (password.value === '') {
-		showError(password, 'Username password is required');
-	} else {
-		showSuccess(password);
-	}
-	if (retypedPassword.value === '') {
-		showError(retypedPassword, 'Password confirmation is required');
-	} else {
-		showSuccess(retypedPassword);
-	}
+	checkRequire([username, email, password, retypedPassword]);
+	checkLength(username, 3, 15);
+	checkLength(password, 6, 25);
+	checkEmail(email);
 });
